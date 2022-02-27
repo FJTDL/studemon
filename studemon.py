9a434c7269e39640034e0e5e0fb43e0e5ea13989
FJTDL <finn.jt.delange@gmail.com>
2022-02-27 21:00:37 +1300

    Creation of students is complete, maybe add names. Next will be testing this and actions with the combat system

--- studemon.py
+++ studemon.py
@@ -2,23 +2,20 @@ from random import randint as rd
 
 # Define functions
 
-def create_student():
-  
-
 def start():
   valid = False
   while not valid:
       global size
       global experience
-      global special
+      global status
       size = int(input("How large is your team?"))
       experience = int(input("How long have you held your position?"))
-      special = input("Are you a dean, HOD or deputy principle? yes/no. ").lower()
-      if special == "yes":
-        special = True
-      elif special == "no":
-        special = False
-      if type(size) == int and type(experience) == int and type(special) == bool:
+      status = input("Are you a dean, HOD or deputy principle? yes/no. ").lower()
+      if status == "yes":
+        status = True
+      else:
+        status = False
+      if type(size) == int and type(experience) == int and type(status) == bool:
         valid = True
       else:
         start()
@@ -28,11 +25,16 @@ def start():
 class Student:
 
   # Initial method
-  def __init__(self, year, academics, involvement, special = False):
+  def __init__(self, year, academics, involvement, status = False):
     self.year = year
     self.smarts = academics
     self.pillar = involvement
-    self.status = special
+    self.status = status
+
+  
+  def __repr__(self):
+    represent = f"This student is year {self.year}, academics of {self.smarts} with an involvement score of {self.pillar} and a leadership status of {self.status}"
+    return represent
   
   # Actions
   def evade():
@@ -55,33 +57,60 @@ class Student:
 class Teacher:
 
   # initialise method
-  def __init__(self, experience, special, size):
+  def __init__(self, experience, status, size):
     self.experience = experience
-    self.special = special
+    self.status = status
     self.size = size
     self.team = []
 
   # Add students to team
   def add(self, student):
     self.team.append(student)
+  
+  def create_student(self):
+    year = int(input("Student year:"))
+    smarts = int(input("Academic ability:"))
+    pillar = int(input("Involvement: "))
+    status = input("Leadership position? yes/no.").lower()
+    if status == "yes":
+      status = True
+    else:
+      status = False
+    while len(self.team) < self.size:
+      if len(self.team) == 0:
+        self.student1 = Student(year, smarts, pillar, status)
+        self.team.append(self.student1)
+      elif len(self.team) == 1:
+        self.student2 = Student(year, smarts, pillar, status)
+        self.team.append(self.student2)
+      else:
+        self.student3 = Student(year, smarts, pillar, status)
+        self.team.append(self.student3)
 
 
-
+print("WELCOME TO STUDEMON (pending legal approval)")
+print("For most of the setup, you will enter numbers 0-10 inclusive unless otherwise stated 😁")
 
 # Code to start the game
 start()
 
 # initialise the opponents
-player1 = Teacher(experience, special, size)
-if player1.special == True:
+player1 = Teacher(experience, status, size)
+if player1.status == True:
   pos = "HEIGHTENED"
 else:
   pos = "STANDARD"
 print(f"PLAYER ONE CREATED, WITH {player1.experience} YEARS OF EXPERIENCE, A {pos} POSITION, AND A TEAM OF {player1.size}.\n")
 start()
-player2 = Teacher(experience, special, size)
+player2 = Teacher(experience, status, size)
 print(f"PLAYER TWO CREATED, WITH {player2.experience} YEARS OF EXPERIENCE, A {pos} POSITION, AND A TEAM OF {player2.size}.\n")
 
 # add students
 
 print("PLAYER ONE, ADD YOUR STUDENTS")
+
+for i in range(0, player1.size):
+  player1.create_student()
+
+for i in range (0, len(player1.team)):
+  print(player1.team[i])
