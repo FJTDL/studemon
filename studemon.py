76cad65afb5f893155dad3e9f6798c38e27a3e7d
FJTDL <finn.jt.delange@gmail.com>
2022-02-27 20:38:19 +1300

    added comments and imported randint to work on actions

--- studemon.py
+++ studemon.py
@@ -1,34 +1,9 @@
-class Student:
+from random import randint as rd
 
-  def __init__(self, year, academics, involvement, special = False):
-    self.year = year
-    self.smarts = academics
-    self.pillar = involvement
-    self.status = special
+# Define functions
+
+def create_student():
   
-
-  def evade():
-    # evade system
-    pass
-
-  
-  def attack():
-    # attack
-    pass
-
-
-  def defend():
-    # defense
-    pass
-
-
-class Teacher:
-
-  def __init__(self, experience, special, size):
-    self.experience = experience
-    self.special = special
-    self.size = size
-
 
 def start():
   valid = False
@@ -49,9 +24,54 @@ def start():
         start()
 
 
+# Declare student class
+class Student:
 
- 
+  # Initial method
+  def __init__(self, year, academics, involvement, special = False):
+    self.year = year
+    self.smarts = academics
+    self.pillar = involvement
+    self.status = special
+  
+  # Actions
+  def evade():
+    # evade system
+    pass
+
+  
+  def attack():
+    # attack
+    pass
+
+
+  def defend():
+    # defense
+    pass
+
+
+# Declare Teacher class
+
+class Teacher:
+
+  # initialise method
+  def __init__(self, experience, special, size):
+    self.experience = experience
+    self.special = special
+    self.size = size
+    self.team = []
+
+  # Add students to team
+  def add(self, student):
+    self.team.append(student)
+
+
+
+
+# Code to start the game
 start()
+
+# initialise the opponents
 player1 = Teacher(experience, special, size)
 if player1.special == True:
   pos = "HEIGHTENED"
@@ -61,3 +81,7 @@ print(f"PLAYER ONE CREATED, WITH {player1.experience} YEARS OF EXPERIENCE, A {po
 start()
 player2 = Teacher(experience, special, size)
 print(f"PLAYER TWO CREATED, WITH {player2.experience} YEARS OF EXPERIENCE, A {pos} POSITION, AND A TEAM OF {player2.size}.\n")
+
+# add students
+
+print("PLAYER ONE, ADD YOUR STUDENTS")
