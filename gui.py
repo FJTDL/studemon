9ed54f12ab22b17ed1b153b9122b19c89a3da7da
FJTDL <finn.jt.delange@gmail.com>
2022-06-11 19:50:06 +1200

    Started learning tkinter in the hopes of making a GUI for the game. gui.py is where I am learning

---
+++ gui.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+root.title("First local trial")
+
+welcome = Label(root, text="WELCOME TO STUDEMON!")
+welcome.pack()
+
+explanation = Label(root, text="Studemon lets you mimic your actual students as a teacher and then pit them in dog fights against each other! Legally!")
+explanation.pack()
+
+root.mainloop()
